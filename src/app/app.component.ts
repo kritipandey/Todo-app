92c0688568a1c04836f25b6c94d91142179d9e48
Pandey <kriti.b.pandey@accenture.com>
2020-12-28 22:21:48 +0530

    'addingFiles'

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'todo-app';
+export class AppComponent { 
+  @ViewChild('f') todoForm: NgForm ;
+  todo = '';
+  todoList = [];
+
+  onSubmit(form:NgForm){
+    this.todoList.push(this.todo);
+    this.todoForm.reset(form);
+    return this.todoList; 
+  }
+
+  onDelete(index){
+    this.todoList.splice(index, 1);
+  }
 }
